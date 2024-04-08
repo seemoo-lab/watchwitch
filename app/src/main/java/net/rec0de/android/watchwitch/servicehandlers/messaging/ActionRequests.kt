4fe4f9ae5b3b0954263897f07343a51dc9518d1d
Nils Rollshausen <nrollshausen@seemoo.tu-darmstadt.de>
2024-04-08 14:41:35 +0200

    fix bulletin action parsing

--- app/src/main/java/net/rec0de/android/watchwitch/servicehandlers/messaging/ActionRequests.kt
+++ app/src/main/java/net/rec0de/android/watchwitch/servicehandlers/messaging/ActionRequests.kt
@@ -1,6 +1,8 @@
 package net.rec0de.android.watchwitch.servicehandlers.messaging
 
 import net.rec0de.android.watchwitch.PBParsable
+import net.rec0de.android.watchwitch.decoders.bplist.CodableBPListObject
+import net.rec0de.android.watchwitch.decoders.protobuf.ProtoBPList
 import net.rec0de.android.watchwitch.decoders.protobuf.ProtoBuf
 import net.rec0de.android.watchwitch.decoders.protobuf.ProtoLen
 import net.rec0de.android.watchwitch.decoders.protobuf.ProtoString
@@ -19,16 +21,16 @@ abstract class AbstractActionRequest(
         return "$name(pubID $publisherBulletinID, recID $recordID, secID $sectionID, actionInfo $actionInfo)"
     }
 
-    fun renderProtobuf(): ByteArray {
+    open fun renderProtobuf(): ByteArray {
         val fields = mutableMapOf<Int,List<ProtoValue>>()
         if(publisherBulletinID != null)
             fields[1] = listOf(ProtoString(publisherBulletinID))
         if(recordID != null)
-            fields[3] = listOf(ProtoString(recordID))
+            fields[2] = listOf(ProtoString(recordID))
         if(sectionID != null)
-            fields[4] = listOf(ProtoString(sectionID))
+            fields[3] = listOf(ProtoString(sectionID))
         if(actionInfo != null)
-            fields[5] = listOf(ProtoLen(actionInfo.renderProtobuf()))
+            fields[4] = listOf(ProtoLen(actionInfo.renderProtobuf()))
 
         return ProtoBuf(fields).renderStandalone()
     }
@@ -73,6 +75,7 @@ class SnoozeActionRequest(
 }
 
 class SupplementaryActionRequest(
+    val identifier: String?,
     publisherBulletinID: String?,
     recordID: String?,
     sectionID: String?,
@@ -81,13 +84,34 @@ class SupplementaryActionRequest(
     override val name = "SupplementaryActionRequest"
     companion object : PBParsable<SupplementaryActionRequest>() {
         override fun fromSafePB(pb: ProtoBuf): SupplementaryActionRequest {
-            val publisherBulletinID = pb.readOptString(1)
-            val recordID = pb.readOptString(2)
-            val sectionID = pb.readOptString(3)
-            val actionInfo = ActionInfo.fromPB(pb.readOptPB(4))
+            val identifier = pb.readOptString(1)
+            val publisherBulletinID = pb.readOptString(2)
+            val recordID = pb.readOptString(3)
+            val sectionID = pb.readOptString(4)
+            val actionInfo = ActionInfo.fromPB(pb.readOptPB(5))
 
-            return SupplementaryActionRequest(publisherBulletinID, recordID, sectionID, actionInfo)
+            return SupplementaryActionRequest(identifier, publisherBulletinID, recordID, sectionID, actionInfo)
         }
+    }
+
+    override fun toString(): String {
+        return "$name(id $identifier, pubID $publisherBulletinID, recID $recordID, secID $sectionID, actionInfo $actionInfo)"
+    }
+
+    override fun renderProtobuf(): ByteArray {
+        val fields = mutableMapOf<Int,List<ProtoValue>>()
+        if(identifier != null)
+            fields[1] = listOf(ProtoString(identifier))
+        if(publisherBulletinID != null)
+            fields[2] = listOf(ProtoString(publisherBulletinID))
+        if(recordID != null)
+            fields[3] = listOf(ProtoString(recordID))
+        if(sectionID != null)
+            fields[4] = listOf(ProtoString(sectionID))
+        if(actionInfo != null)
+            fields[5] = listOf(ProtoLen(actionInfo.renderProtobuf()))
+
+        return ProtoBuf(fields).renderStandalone()
     }
 }
 
@@ -98,38 +122,38 @@ class AcknowledgeActionRequest(
     actionInfo: ActionInfo?
 ) : AbstractActionRequest(publisherBulletinID, recordID, sectionID, actionInfo) {
     override val name = "AcknowledgeActionRequest"
-    companion object : PBParsable<SupplementaryActionRequest>() {
-        override fun fromSafePB(pb: ProtoBuf): SupplementaryActionRequest {
+    companion object : PBParsable<AcknowledgeActionRequest>() {
+        override fun fromSafePB(pb: ProtoBuf): AcknowledgeActionRequest {
             val publisherBulletinID = pb.readOptString(1)
             val recordID = pb.readOptString(2)
             val sectionID = pb.readOptString(3)
             val actionInfo = ActionInfo.fromPB(pb.readOptPB(4))
 
-            return SupplementaryActionRequest(publisherBulletinID, recordID, sectionID, actionInfo)
+            return AcknowledgeActionRequest(publisherBulletinID, recordID, sectionID, actionInfo)
         }
     }
 }
 
-class ActionInfo(val context: ByteArray?, val contextNulls: ByteArray?) {
+class ActionInfo(val context: CodableBPListObject?, val contextNulls: CodableBPListObject?) {
     override fun toString(): String {
-        return "ActionInfo(context: ${context?.hex()}, contextNulls: ${contextNulls?.hex()})"
+        return "ActionInfo(context: $context, contextNulls: $contextNulls})"
     }
 
     companion object : PBParsable<ActionInfo>() {
         override fun fromSafePB(pb: ProtoBuf): ActionInfo {
-            val context = pb.readOptionalSinglet(4) as ProtoLen?
-            val contextNulls = pb.readOptionalSinglet(5) as ProtoLen?
+            val context = pb.readOptionalSinglet(4) as ProtoBPList?
+            val contextNulls = pb.readOptionalSinglet(5) as ProtoBPList?
 
-            return ActionInfo(context?.value, contextNulls?.value)
+            return ActionInfo(context?.parsed as CodableBPListObject?, contextNulls?.parsed as CodableBPListObject?)
         }
     }
 
     fun renderProtobuf(): ByteArray {
         val fields = mutableMapOf<Int,List<ProtoValue>>()
         if(context != null)
-            fields[4] = listOf(ProtoLen(context))
+            fields[4] = listOf(ProtoLen(context.renderAsTopLevelObject()))
         if(contextNulls != null)
-            fields[5] = listOf(ProtoLen(contextNulls))
+            fields[5] = listOf(ProtoLen(contextNulls.renderAsTopLevelObject()))
 
         return ProtoBuf(fields).renderStandalone()
     }
